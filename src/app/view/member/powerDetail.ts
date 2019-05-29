18afd2a31e824223a422bde0f08cecd29d276bcf
limh <justsmileli@163.com>
2019-05-29 20:15:44 +0800

    HGYH-253

--- src/app/view/member/powerDetail.ts
+++ src/app/view/member/powerDetail.ts
@@ -1,4 +1,5 @@
 import { popNew } from '../../../pi/ui/root';
+import { Forelet } from '../../../pi/widget/forelet';
 import { Widget } from '../../../pi/widget/widget';
 import { upgradeHWang } from '../../net/pull';
 import { getStore, register, UserType } from '../../store/memstore';
@@ -10,16 +11,18 @@ interface Props {
     userType:UserType;  // 用户会员等级 
     powerList:any[];  // 权益列表
     userTypeShow:string;  // 用户类型名称
-    code:string;  // 邀请码
-    upgrade:boolean;  // 是否弹出填信息弹窗
 }
-
+// tslint:disable-next-line:no-reserved-keywords
+declare var module: any;
+export const forelet = new Forelet();
+export const WIDGET_NAME = module.id.replace(/\//g, '-');
 /**
  * 权益详情
  */
 export class PowerDetail extends Widget {
     public ok:() => void;
     public props:Props;
+    public state:string = '';  // 自己的邀请码
    
     public setProps(props:any) {
         super.setProps(props);
@@ -31,12 +34,9 @@ export class PowerDetail extends Widget {
             this.props.powerList = Constant.hBaoPower;
         }
         if (getStore('user/userType') === props.userType) {
-            this.props.code = getStore('user/inviteCode','');
+            this.state = getStore('user/inviteCode','');
         }
         this.props.userTypeShow = getUserTypeShow(props.userType);
-        if (props.upgrade) {
-            this.upgradeUser();
-        }
     }
 
     // 权益
@@ -49,21 +49,30 @@ export class PowerDetail extends Widget {
 
     // 升级会员等级
     public upgradeUser() {
-        popNew('app-view-member-applyModalBox',null,(sel) => {
-            if (this.props.userType === UserType.hWang) {
-                upgradeHWang(sel).then(() => {
-                    popNewMessage('成功发送海王申请');
-                });
-            } else {
-                payToUpHbao(sel,() => {
-                    this.ok && this.ok();
-                });
-                register('flags/upgradeHbao',() => {
+        if (!this.state) {  // 没有邀请码，不是当前等级的会员可以开通
+            popNew('app-view-member-applyModalBox',null,(sel) => {
+                if (this.props.userType === UserType.hWang) {
+                    upgradeHWang(sel).then(() => {
+                        popNewMessage('成功发送海王申请');
+                    });
+                } else {
                     payToUpHbao(sel,() => {
                         this.ok && this.ok();
                     });
-                });
-            }
-        });
+                    register('flags/upgradeHbao',() => {
+                        payToUpHbao(sel,() => {
+                            this.ok && this.ok();
+                        });
+                    });
+                }
+            });
+        }
     }
 }
+
+register('user',(r) => {
+    const w = forelet.getWidget(WIDGET_NAME);
+    if (r.userType === w.props.userType) {
+        forelet.paint(r.inviteCode);
+    }
+});
