a0bea0d194445e3cce01b99911efeb9a4659cea3
limh <justsmileli@163.com>
2019-05-31 09:43:22 +0800

    HGYH-199

--- src/app/utils/logic.ts
+++ src/app/utils/logic.ts
@@ -147,6 +147,12 @@ export const payToUpHbao = (sel:string,cb?:any) => {
             });
             getAllGifts();  // 重新获取所有礼包
             cb && cb();
+        }).catch(err => {
+            if (err.result === 4012) {
+                popNewMessage('获取上级失败');
+            } else {
+                popNewMessage('升级海宝失败');
+            }
         });
     }
 };
@@ -160,6 +166,8 @@ export const applyToUpHwang = (sel:string) => {
     }).catch(err => {
         if (err.result === 4007) {
             popNewMessage('申请已在处理中');
+        } else if (err.result === 4012) {
+            popNewMessage('获取上级失败');
         } else {
             popNewMessage('发送海王申请失败');
         }
