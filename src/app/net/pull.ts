08c29960e2fcac0c8ed9fcf963630fe204ef344e
limh <justsmileli@163.com>
2019-08-05 12:06:42 +0800

    HGYH-571 支付接口ext改为string类型

--- src/app/net/pull.ts
+++ src/app/net/pull.ts
@@ -770,7 +770,7 @@ export const payMoney = (money:number,ttype:string,count:number= 1,ext?:any,fail
             type:ttype,
             count,
             channel,
-            ext
+            ext:JSON.stringify(ext)
         }
     };
 
